a22c50e837b6bd758c6aed56d9e90035e34004d2
NatC02 <natanceballos02@gmail.com>
2023-01-26 00:00:53 -0500

    mofified script

--- js/active-url-link.js
+++ js/active-url-link.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const links = document.querySelectorAll("li a");
+const links = document.querySelectorAll("nav ul li a");
 const currentUrl = window.location.pathname;
 
 console.log("current page " + currentUrl);
@@ -10,17 +10,17 @@ links.forEach(function(link) {
   // or if its the root url map to /index.html instead
   (currentUrl === "/" && link.getAttribute("href") === "/index.html")
   ) {
-    link.className = "";
+    // link.className = "";
     // link.classList.add("link-active");
     // link.classList.remove("link:hover");
     // console.log("active link " + link)
     // add a click event listener to the link
 
-    link.classList.add("link-active","link-active:active");
+    // link.classList.add("link-active","link-active:active");
+    link.classList.toggle("link-active");
 
     link.addEventListener("click", event => {
       event.preventDefault(); // prevent default behavior
-      // add a class to the link
     });
   } else {
     link.classList.remove("link-active");
